3e9301a93e0721d1708814270f7659370684e11b
unknown <victorst79@gmail.com>
2018-11-20 10:20:36 +0100

    change in function

--- Unit 1/Worksheet#8/index.php
+++ Unit 1/Worksheet#8/index.php
@@ -5,44 +5,40 @@
     <title>Document</title>
 </head>
 <body>
+    <h1>Formulario</h1>
     <form action="#" method="POST" enctype="multipart/form-data">
-        <input type="text" name="nombre">
-        <input type="text" name="surname">
-        <input type="date" name="date">
-        <input type="submit">
+        <label>Usuario</label>
+        <br>
+        <input type="text" name="usuario">
+        <br>
+        <label>Contraseña</label>
+        <br>
+        <input type="password" name="password">
+        <br>
+        <label>E-Mail</label>
+        <br>
+        <input type="email" name="email">
+        <br>
+        <input type="submit" name="envio">
         <input type="reset">
     </form>
     
     <?php
-        class Person{
-            private $firstName;
-            private $lastName;
-            private $dateOfBirth;
+         $users = array();   
 
-            function __construct($firstName = 'John', $lastName = 'Doe', $dateOfBirth = '1959-01-01'){
-                $this->firstName = $firstName;
-                $this->lastName = $lastName;
-                $this->dateOfBirth = $dateOfBirth;
-            }
+        function creacionUsuario($usuario,$pass,$email){
+            $users[$usuario] = [
+                "Contraseña" => $pass,
+                "Email" => $email
+            ];
+        }
 
-            function getDateOfBirth(){
-                return $this->dateOfBirth;
-            }
-        
-            public function setDateOfBirth($date){
-                $this->dateOfBirth = $date;
-            }
+        if(isset($_POST["envio"])){
+            $usuario = $_POST["usuario"];
+            $pass = $_POST["password"];
+            $email = $_POST["email"];
 
-            public function fullName(){
-                return "{$this->firstName} {$this->lastName}";
-            }
-
-            public function age(){
-                $today = date('Y-m-d');
-                $diff = strtotime($today) - strtotime($this->dateOfBirth);
-                $years = floor($diff / (365*60*60*24));
-                return $years;
-            }
+            creacionUsuario($usuario,$pass,$email);
         }
     ?>
 </body>
